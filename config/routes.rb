6c866e2589bd5acc4caa868f3570a5453a019d37
ack43 <i43ack@gmail.com>
2014-05-28 12:50:18 +0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RailsAdmin::Engine.routes.draw do
   controller "main" do
     scope ":model_name" do
       scope "(:id)/:field" do
-        get "/jcrop", :to => 'jcrop#edit', :as => :jcrop
+        post "/jcrop", :to => 'jcrop#edit', :as => :jcrop
         put "/jcrop", :to => 'jcrop#update'
       end
     end
